c9f4f262deccab23634c2bfe2365b8a3282b4674
Shweta Srivastava <shweta.srivastava2394@gmail.com>
2020-04-12 15:11:51 -0400

    Updates

---
+++ precache-manifest.87c978f64cc9f4f8f157415da5657e16.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d3e863cb0e71e9b02c5813cc3bf3c6b",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "4fadfc14aae566c36ba8",
+    "url": "/my-portfolio/static/css/main.dc0a14bb.chunk.css"
+  },
+  {
+    "revision": "0a5470f1e1732c05ed01",
+    "url": "/my-portfolio/static/js/2.e9c0c096.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-portfolio/static/js/2.e9c0c096.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4fadfc14aae566c36ba8",
+    "url": "/my-portfolio/static/js/main.2474011e.chunk.js"
+  },
+  {
+    "revision": "6d7a7337562760299d40",
+    "url": "/my-portfolio/static/js/runtime-main.00bba678.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-portfolio/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "3bc32de4584f8040f9100b6b08457531",
+    "url": "/my-portfolio/static/media/puzzle.3bc32de4.jpg"
+  },
+  {
+    "revision": "4bfab9faf28cee096105480404d92eb3",
+    "url": "/my-portfolio/static/media/puzzle2.4bfab9fa.jpeg"
+  },
+  {
+    "revision": "17469043649422af75bfcfab8a99cd1a",
+    "url": "/my-portfolio/static/media/puzzle3.17469043.jpeg"
+  },
+  {
+    "revision": "e353b8df1a8c0cf7a7c9003cc4750341",
+    "url": "/my-portfolio/static/media/puzzle4.e353b8df.jpeg"
+  }
+]);
